b2cada38bb4060208933e487003a113a5901520d
Abhishek Singh <abhis1997singh@gmail.com>
2024-04-08 00:53:01 -0500

    wip

--- src/main.py
+++ src/main.py
@@ -22,73 +22,95 @@ from flask import send_file
 import pytz
 
 
-app = Flask("healthstaffconnect")
-
 logging.basicConfig(level=logging.INFO)
-# load_dotenv(".env")
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "medjunction-8df36356d2d0.json" 
-# db = firestore.Client(project="healthstaffconnect")
-# app = Flask("healthstaffconnect")
-bucket_name = "paitent_profile_bucket"
-
-session_uuid = str(uuid1())
 # Get current Indian Standard Time (IST)
 ist = pytz.timezone('Asia/Kolkata')
 current_time = datetime.now(ist)
 
-
-storage_client = storage.Client()
 # Specify the allowed file extensions
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif", "pdf"}
 
+app = Flask("medjunction")
 
+session_uuid = str(uuid1())
 # Set the path where uploaded files will be saved
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+storage_client = storage.Client()
+bucket_name = "paitent_profile_bucket"
+db = firestore.Client(project="medjunction")
+paitent_collection = db.collection("paitent")
+doc_ref = paitent_collection.document("2")
+# doc_ref.set(
+#     {
+#     "name": "Abhis Doe",
+#     "sex": "Male",
+#     "location": "Gorakhpur, India",
+#     "insurance_provider": "star",
+#     "insurance": "AKJKJSDF123R3",
+# }
+# )
+# doc_ref.update(
+#     {"complaints_data":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
 
-# Sample initial patient data
-patient_data = {
-    "name": "Abhis Doe",
-    "sex": "Male",
-    "location": "Gorakhpur, India",
-    "insurance_provider": "star",
-    "insurance": "AKJKJSDF123R3",
-}
-complaints_data = [
-    "27/Mar/2024: Complaint 1, Complaint 2",
-    "29/Mar/2024:  Complaint 3",
-]  # Example data, replace it with your actual data
+# doc_ref.update(
+#     {"co_morbidities":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
 
-co_morbidities = [
-    "27/Mar/2024: Complaint 1, Complaint 2",
-    "29/Mar/2024:  Complaint 3",
-]  # Example data, replace it with your actual data
 
-hopi = [
-    "27/Mar/2024: Complaint 1, Complaint 2",
-    "29/Mar/2024:  Complaint 3",
-]  # Example data, replace it with your actual data
-surgery_history = [
-    "27/Mar/2024: Complaint 1, Complaint 2",
-    "29/Mar/2024:  Complaint 3",
-]
-addiction_history = [
-    "27/Mar/2024: Complaint 1, Complaint 2",
-    "29/Mar/2024:  Complaint 3",
-]
-past_history = [
-    "27/Mar/2024: Complaint 1, Complaint 2",
-    "29/Mar/2024:  Complaint 3",
-]
-reports = [
-    "29/Mar/2024:  Complaint 3",
-    "27/Mar/2024: Complaint 1, Complaint 2",
-]
+# doc_ref.update(
+#     {"hopi":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
+
+# doc_ref.update(
+#     {"surgery_history":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
+
+# doc_ref.update(
+#     {"addiction_history":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
+
+# doc_ref.update(
+#     {"past_history":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
+
+
+# doc_ref.update(
+#     {"reports":["27/Mar/2024: Complaint 1, Complaint 2",
+#     "29/Mar/2024:  Complaint 3",]}
+# )
 
 
 @app.route("/")
 def index():
+
+    doc = doc_ref.get()
+    print(doc)
+    if doc.exists:
+        patient_data = {"name": doc.get("name"),
+        "insurance": doc.get("insurance"),
+        "insurance_provider": doc.get("insurance_provider"),
+        "location": doc.get("location"),
+        "sex": doc.get("sex")
+        }
+
+        complaints_data = doc.get("complaints_data")
+        co_morbidities = doc.get("co_morbidities")
+        past_history = doc.get("past_history")
+        addiction_history = doc.get("addiction_history")
+        surgery_history = doc.get("surgery_history")
+        reports = doc.get("reports")
+    else:
+        return "Profile does not exists"
     return render_template(
         "patient_profile.html",
         patient=patient_data,
@@ -102,24 +124,79 @@ def index():
 
 @app.route('/edit_profile_page')
 def edit_profile_page():
+    doc = doc_ref.get()
+
+    if doc.exists:
+        patient_data = {"name": doc.get("name"),
+        "insurance": doc.get("insurance"),
+        "insurance_provider": doc.get("insurance_provider"),
+        "location": doc.get("location"),
+        "sex": doc.get("sex")
+        }
     return render_template('edit_profile.html',patient=patient_data)
 
+
+@app.route("/edit-profile", methods=["GET", "POST"])
+def edit_profile():
+    if request.method == "POST":
+        doc_ref.update(
+        {
+        "name": request.form["name"],
+        "sex": request.form["sex"],
+        "location": request.form["location"],
+        "insurance_provider": request.form["insurance"],
+        "insurance": request.form["insurance_provider"],
+        })
+        doc = doc_ref.get()
+        if doc.exists:
+            patient_data = {"name": doc.get("name"),
+            "insurance": doc.get("insurance"),
+            "insurance_provider": doc.get("insurance_provider"),
+            "location": doc.get("location"),
+            "sex": doc.get("sex")
+            }
+        # Redirect to the profile page after editing
+        return redirect(url_for("index"))
+    # If it's a GET request, just render the edit form
+    return render_template("edit_profile.html", patient=patient_data)
 
 @app.route("/save_text", methods=["POST"])
 def save_text():
     data = request.json
     text = data.get("text")
     source = data.get("source")
+    doc = doc_ref.get()
+
     if source == "complaints-new":
-        complaints_data.append(text)
+        if doc.exists:
+            complaints_data=doc.get("complaints_data")
+            complaints_data.append(text)
+            doc_ref.update({"complaints_data": complaints_data})
+        
     elif source == "co-morbidities-new":
-        co_morbidities.append(text)
+        if doc.exists:
+            co_morbidities=doc.get("co_morbidities")
+            co_morbidities.append(text)
+            doc_ref.update({"co_morbidities": co_morbidities})
+        
     elif source == "surgery-history-new":
-        surgery_history.append(text)
+        if doc.exists:
+            surgery_history=doc.get("surgery_history")
+            surgery_history.append(text)
+            doc_ref.update({"surgery_history": surgery_history})
+        
     elif source == "past-history-new":
-        past_history.append(text)
+        if doc.exists:
+            past_history=doc.get("past_history")
+            past_history.append(text)
+            doc_ref.update({"past_history": past_history})
+        
     elif source == "addiction-history-new":
-        addiction_history.append(text)
+        if doc.exists:
+            addiction_history=doc.get("addiction_history")
+            addiction_history.append(text)
+            doc_ref.update({"addiction_history": addiction_history})
+        
     # Here you can save the text to your database or perform any other required action
     print("Received text:", text)
     return jsonify({"success": True})
@@ -130,46 +207,41 @@ def discard_text():
     data = request.json
     text = data.get("text")
     source = data.get("source")
-    print(source)
+    doc = doc_ref.get()
     if source == "complaints-new":
-        for i in reversed(range(len(complaints_data))):
-            if text in complaints_data[i]:
-                complaints_data.pop(i)
+        if doc.exists:
+            complaints_data=doc.get("complaints_data")
+            for i in reversed(range(len(complaints_data))):
+                if text in complaints_data[i]:
+                    complaints_data.pop(i)
+            doc_ref.update({"complaints_data": complaints_data})
     elif source == "co-morbidities-new":
+        co_morbidities=doc.get("co_morbidities")
         for i in reversed(range(len(co_morbidities))):
             if text in co_morbidities[i]:
                 co_morbidities.pop(i)
+        doc_ref.update({"co_morbidities": co_morbidities})
     elif source == "surgery-history-new":
+        surgery_history=doc.get("surgery_history")
         for i in reversed(range(len(surgery_history))):
             if text in surgery_history[i]:
                 surgery_history.pop(i)
+        doc_ref.update({"surgery_history": surgery_history})
     elif source == "past-history-new":
+        past_history=doc.get("past_history")
         for i in reversed(range(len(past_history))):
             if text in past_history[i]:
                 past_history.pop(i)
+        doc_ref.update({"past_history": past_history})
     elif source == "addiction-history-new":
+        addiction_history=doc.get("addiction_history")
         for i in reversed(range(len(addiction_history))):
             if text in addiction_history[i]:
                 addiction_history.pop(i)
+        doc_ref.update({"addiction_history": addiction_history})
 
     print("Discarded text:", text)
     return jsonify({"success": True})
-
-
-@app.route("/edit-profile", methods=["GET", "POST"])
-def edit_profile():
-    if request.method == "POST":
-        # Update patient data with form data
-        patient_data["name"] = request.form["name"]
-        patient_data["sex"] = request.form["sex"]
-        patient_data["location"] = request.form["location"]
-        patient_data["insurance"] = request.form["insurance"]
-        patient_data["insurance_provider"] = request.form["insurance_provider"]
-        # Redirect to the profile page after editing
-        return redirect(url_for("index"))
-    # If it's a GET request, just render the edit form
-    return render_template("edit_profile.html", patient=patient_data)
-
 
 @app.route("/upload", methods=["POST"])
 def upload_file():
